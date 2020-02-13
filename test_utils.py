5084e72ba837d0c69c24db1bdb18f8a795e60f42
Sarah Liettefti <sarah.liettefti@gmail.com>
2020-02-13 10:42:00 +0100

    Add unit test for utils module

--- test_utils.py
+++ test_utils.py
@@ -8,15 +8,20 @@ import utils
 class TestUtils(unittest.TestCase):
     def test_fact(self):
         # À compléter...
-        pass
+        self.assertEqual(utils.fact(2), 2)
+        self.assertEqual(utils.fact(0), 1)
+        with self.assertRaises(ValueError):
+            utils.fact(-1)
     
     def test_roots(self):
         # À compléter...
-        pass
+        self.assertEqual(utils.roots(-1, 0, 1), (-1, 1))
+        self.assertEqual(utils.roots(1, -4, 4), (2))
+        self.assertEqual(utils.roots(1, 2, 2), ())
     
     def test_integrate(self):
         # À compléter...
-        pass
+        self.assertEqual(utils.integrate("2*x", -4, 2), 12)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
